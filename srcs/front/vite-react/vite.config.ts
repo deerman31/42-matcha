235b6b8e9eeea287fcaaabd38c69e01ae0a84056
Yoshinari <110902237+deerman31@users.noreply.github.com>
2024-12-30 15:40:36 +0900

    フロントの最低限の環境構築はできた。

---
+++ srcs/front/vite-react/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // これが重要です
+    //port: 5173,
+    port: process.env.FRONT_PORT ? parseInt(process.env.FRONT_PORT) : 5173,
+    watch: {
+      usePolling: true
+    }
+  }
+})
