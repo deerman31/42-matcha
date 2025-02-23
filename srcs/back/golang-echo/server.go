3f9032f092384b120a9cb7c4f4576205cd85cea2
Yoshinari <110902237+deerman31@users.noreply.github.com>
2025-02-24 01:25:37 +0900

    swaggerを設定

--- srcs/back/golang-echo/server.go
+++ srcs/back/golang-echo/server.go
@@ -10,8 +10,17 @@ import (
 
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq" // PostgreSQLドライバー
+
+	_ "golang-echo/docs" // swag initで生成されるdocsパッケージ
+
+	echoSwagger "github.com/swaggo/echo-swagger" // Swaggerパッケージの追加
 )
 
+// @title あなたのAPIタイトル
+// @version 1.0
+// @description APIの説明をここに書く
+// @host localhost:3000
+// @BasePath /api/v1
 func main() {
 	// データベースに接続
 	db, err := database.ConnectDB()
@@ -22,6 +31,9 @@ func main() {
 	fmt.Println("Successfully connected to the database")
 
 	e := echo.New()
+
+	// Swaggerのルート設定
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	// ミドルウェアの設定
 	middle.SetupMiddleware(e)
